dfbd16d29e1a8b98e512f652d0274597289e9fae
Christian Duran <cduran17@apu.edu>
2021-03-08 18:27:05 -0800

    Level 5 Added
    Level 5 created. Adding items tomorrow.

--- Game230/Platformer/Assets/SCRIPTS/Pickups.cs
+++ Game230/Platformer/Assets/SCRIPTS/Pickups.cs
@@ -6,8 +6,13 @@ public class Pickups : MonoBehaviour
 {
     [SerializeField] AudioClip coinPickSFX;
 
+    [SerializeField] int coinValue = 100;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
+        FindObjectOfType<GameSession>().ProcessPlayerScore(coinValue);
+
         //PlayClipAtPoint uses Vector3 info for where the sound plays from
         AudioSource.PlayClipAtPoint(coinPickSFX, Camera.main.transform.position);
 
