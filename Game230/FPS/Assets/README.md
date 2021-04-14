58fdcf23f395b3347743b209adc909cfc813d26d
Christian Duran <cduran17@apu.edu>
2021-04-14 14:48:36 -0700

    Level Created. Particle Systems. Animations.

---
+++ Game230/FPS/Assets/README.md
@@ -0,0 +1,7 @@
+# GAME 230: 3D FPS Starter Assets
+
+## Description
+3D FPS Starter Assets for GAME 230 Game Development Studio II.
+
+## Instructions
+Click on the green **Code** and then click the **Download ZIP** link. Drag-and-drop the folders into a fresh Unity 3D project.
--- Game230/FPS/Assets/Scripts/EnemyAI.cs
+++ Game230/FPS/Assets/Scripts/EnemyAI.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5.0f;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
 
     NavMeshAgent nMa;
     // Start is called before the first frame update
@@ -16,6 +20,51 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+    else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+
+    }
+
+   
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget >= nMa.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if (distanceToTarget >= nMa.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
+        GetComponent<Animator>().SetBool("Attack", false);
+        GetComponent<Animator>().SetTrigger("Move");
         nMa.SetDestination(target.position);
+    }
+
+    private void AttackTarget()
+    {
+        GetComponent<Animator>().SetBool("Attack", true);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        //display chase radius when selected
+        Gizmos.color = new Color(255f, 0f, 0f);
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+  
     }
 }
